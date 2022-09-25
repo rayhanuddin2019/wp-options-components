48078b8abaf08fed20d471ea930b431e6270d741
Rayhan <rayhanuddin.it.2019@gmail.com>
2022-09-25 19:04:34 +0600

    blog tab issue fix

--- build/blocks/block-1/index.asset.php
+++ build/blocks/block-1/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f6a95900e817b3f5450a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '32af22623accb8dae0dc');
